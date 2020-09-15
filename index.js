718f0d111229ada2422eb53068d84f1a3eac5dbb
fenglisa <lisafeng10@gmail.com>
2020-09-15 16:13:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let element = document.querySelector("main#main");
+element.remove();
+
+let newHeader = document.createElement('h1#victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Lisa is the champion";
