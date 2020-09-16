314bcd40961e2ec4e2e4d5f4c656fe58309151a3
kayla <kkingston09@gmail.com>
2020-09-16 15:58:31 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+
+ main.remove('main#main');
+
+ // new header h1#victory with: "YOUR-NAME is the champion"
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.className = "victory";
+
+
+// let newHeader = document.querySelector('body');
+// newHeader.innerHTML = "<h1 class=victory>YOUR-NAME is the champion</h1>"
+
+
+
+
+
+
+
+
+
+
+
