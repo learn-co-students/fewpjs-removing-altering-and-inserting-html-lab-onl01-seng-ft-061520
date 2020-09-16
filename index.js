63c60a7b435015c3adc563292bcb65823c62d44a
nadavschwartz <nadavschwartz58@gmail.com>
2020-09-16 14:28:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let el = document.getElementById('main')
+el.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Nadav is the champion"
