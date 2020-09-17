cd1721bf7edc79911d1b28734080e5e7e11ee810
Soundwood <j.benhogan@gmail.com>
2020-09-17 10:10:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+main.remove(main.querySelector('main'))
+
+let newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = "Someone is the champion!"
