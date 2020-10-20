b8330337d91863a4143984ed8ebeb9cc1475d075
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-10-20 14:29:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'RAY is the champion';
+document.body.append(newHeader);
